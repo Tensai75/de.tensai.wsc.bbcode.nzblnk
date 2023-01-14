0cca0d8a33d768878c8d4d2682aa341ea829145a
Tensai <tensai@gmx.net>
2023-01-14 12:19:52 +0100

    Simplify regexp and add parameter d

--- src/files/lib/system/bbcode/nzblnkBBCode.class.php
+++ src/files/lib/system/bbcode/nzblnkBBCode.class.php
@@ -24,20 +24,29 @@ class nzblnkBBCode extends AbstractBBCode{
     public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
         if ($parser->getOutputType() == 'text/html') {
             if (is_array($openingTag['attributes']) && count($openingTag['attributes']) > 0 && $openingTag['attributes'][0] != '') {
-                $re = '/(?:(?:(?:&amp;|&|\?)?t=(?<title>.*?)(?=(?:&amp;|&|\?)?(?:[htpg]=)|$))|(?:(?:&amp;|&|\?)?h=(?<header>.*?)(?=(?:&amp;|&|\?)?(?:[htpg]=)|$))|(?:(?:&amp;|&|\?)?p=(?<password>.*?)(?=(?:&amp;|&|\?)?(?:[htpg]=)|$))|(?:(?:&amp;|&|\?)?g=(?<group>.*?)(?=(?:&amp;|&|\?)?(?:[htpg]=)|$)))+/i';
-                preg_match($re, html_entity_decode($openingTag['attributes'][0]), $matches, PREG_UNMATCHED_AS_NULL);
-				if ($matches['header']) {
-					$nzblnk = 'nzblnk://';
-					$nzblnk .= $matches['header'] ? '?h='.rawurlencode(rawurldecode($matches['header'])) : '';
-					$nzblnk .= $matches['title'] ? '&t='.rawurlencode(rawurldecode($matches['title'])) : '';
-					$nzblnk .= $matches['password'] ? '&p='.rawurlencode(rawurldecode($matches["password"])) : '';
-					$nzblnk .= $matches['group'] ? '&g='.rawurlencode(rawurldecode($matches['group'])) : '';
-					WCF::getTPL()->assign([
-							'nzblnk' => $nzblnk,
-							'wcf_path' => (WCF::getPath()),
-					]);
-					return WCF::getTPL()->fetch('nzblnkBBCodeTag');
-				}
+                $re = '/(?:[&?]([htpgd])=([^&]+))/i';
+                preg_match_all($re, html_entity_decode($openingTag['attributes'][0]), $matches, PREG_SET_ORDER);
+                foreach (str_split('htpgd') as $parameter) {
+                    $parameters[$parameter] = [];
+                }
+                foreach ($matches as $values) {
+                    array_push($parameters[$values[1]],$values[2]);
+                }
+                if (count($parameters['h']) > 0) {
+                    $nzblnk = 'nzblnk://?h='.rawurlencode(rawurldecode($parameters['h'][0]));
+                    $nzblnk .= count($parameters['t']) > 0 ? '&t='.rawurlencode(rawurldecode($parameters['t'][0])) : '';
+                    $nzblnk .= count($parameters['p']) > 0 ? '&p='.rawurlencode(rawurldecode($parameters['p'][0])) : '';
+                    if (count($parameters['g']) > 0) {
+                        foreach ($parameters['g'] as $group) {
+                            $nzblnk .= '&g='.rawurlencode(rawurldecode($group));
+                        }
+                    $nzblnk .= count($parameters['d']) > 0 ? '&d='.rawurlencode(rawurldecode($parameters['d'][0])) : '';
+                    WCF::getTPL()->assign([
+                            'nzblnk' => $nzblnk,
+                            'wcf_path' => (WCF::getPath()),
+                    ]);
+                    return WCF::getTPL()->fetch('nzblnkBBCodeTag');
+                }
             }
         }
         return '';
